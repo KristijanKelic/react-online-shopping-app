4a2a2ddc85686de213a9b7d8f0ec1ef76618cab4
Kristijan <kkelic7@gmail.com>
2019-12-23 13:01:33 +0100

    created shop page, collection preview component, started collection item

--- online-shopping/src/App.js
+++ online-shopping/src/App.js
@@ -3,12 +3,16 @@ import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component';
+import HomePage from './pages/homepage/home.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
---
+++ online-shopping/src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './collection-item.styles.scss';
+
+const CollectionItem = () => {
+  return <div></div>;
+};
+
+export default CollectionItem;
