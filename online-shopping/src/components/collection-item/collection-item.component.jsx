c1c9ac8f86d6e16e1f04f53227565662a2db2514
Kristijan <kkelic7@gmail.com>
2019-12-23 14:39:23 +0100

    finishing styling of collection item and preview

--- online-shopping/src/components/collection-item/collection-item.component.jsx
+++ online-shopping/src/components/collection-item/collection-item.component.jsx
@@ -2,8 +2,21 @@ import React from 'react';
 
 import './collection-item.styles.scss';
 
-const CollectionItem = () => {
-  return <div></div>;
+const CollectionItem = ({ id, name, price, imageUrl }) => {
+  return (
+    <div className="collection-item">
+      <div
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+        className="background-image"
+      />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+    </div>
+  );
 };
 
 export default CollectionItem;
