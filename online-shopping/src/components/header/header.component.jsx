c4feb2c50b41adf4197f55b670cc6ef745101c1e
Kristijan <kkelic7@gmail.com>
2019-12-23 15:36:23 +0100

    added header to a project

---
+++ online-shopping/src/components/header/header.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+import './header.styles.scss';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Link to="/" className="logo-container">
+        <Logo className="logo" />
+      </Link>
+      <div className="links-container">
+        <Link to="/shop" className="link">
+          SHOP
+        </Link>
+        <Link to="/contact" className="link">
+          CONTACT
+        </Link>
+        <Link to="/" className="link">
+          SIGN IN
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
