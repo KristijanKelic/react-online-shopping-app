8b376fa06bf4c3061085676b247c64638d44fb6f
Kristijan <kkelic7@gmail.com>
2019-12-24 14:18:23 +0100

    added sign up with email and password functionality

--- online-shopping/src/App.js
+++ online-shopping/src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 import HomePage from './pages/homepage/home.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
-import SignInAndUpPage from './pages/signin/sign-in.component';
+import SignInAndUpPage from './pages/sign-in-up-page/sign-in-up-page.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
