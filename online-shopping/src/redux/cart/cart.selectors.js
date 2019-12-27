7542f55ca3f02a20945b2251ffdb36e84178b984
Kristijan <kkelic7@gmail.com>
2019-12-27 17:13:11 +0100

    installed reselect, replaced cart selectors with memoized selectors to stop components from rerendering upon unrelated state changes

---
+++ online-shopping/src/redux/cart/cart.selectors.js
@@ -0,0 +1,17 @@
+import { createSelector } from 'reselect';
+
+// Input selector
+const selectCart = state => state.cart;
+
+// Creating selector for cartItems
+export const selectCartItems = createSelector(
+  [selectCart],
+  cart => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector([selectCartItems], cartItems =>
+  cartItems.reduce(
+    (accuQuantity, cartItem) => accuQuantity + cartItem.quantity,
+    0
+  )
+);
