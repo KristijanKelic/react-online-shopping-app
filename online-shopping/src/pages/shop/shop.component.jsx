c41aadefe98fb8e4fef456fabe69de741e728e19
Kristijan <kkelic7@gmail.com>
2019-12-28 16:12:55 +0100

    added shop data to redux, removed from local state in shop page

--- online-shopping/src/pages/shop/shop.component.jsx
+++ online-shopping/src/pages/shop/shop.component.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
-
-import SHOP_DATA from './shop.data';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-export default class ShopPage extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherColProps }) => (
-          <CollectionPreview key={id} {...otherColProps} />
-        ))}
-      </div>
-    );
-  }
-}
+import { selectShopCollections } from '../../redux/shop/shop.selectors';
+
+const ShopPage = ({ collections }) => {
+  return (
+    <div className="shop-page">
+      {collections.map(({ id, ...otherColProps }) => (
+        <CollectionPreview key={id} {...otherColProps} />
+      ))}
+    </div>
+  );
+};
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectShopCollections
+});
+
+export default connect(mapStateToProps)(ShopPage);
