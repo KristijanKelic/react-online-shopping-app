9f5733c08af3455ed6400b53fd58f64ce6ae66d0
Kristijan <kkelic7@gmail.com>
2019-12-28 01:48:39 +0100

    added checkout item component to display items in checkout

--- online-shopping/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ online-shopping/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,13 +4,14 @@ import { createStructuredSelector } from 'reselect';
 import { withRouter } from 'react-router-dom';
 
 import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems, history }) => {
+const CartDropdown = ({ cartItems, history, dispatch }) => {
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
@@ -21,7 +22,12 @@ const CartDropdown = ({ cartItems, history }) => {
         )}
       </div>
       {cartItems.length > 0 ? (
-        <CustomButton onClick={() => history.push('/checkout')}>
+        <CustomButton
+          onClick={() => {
+            history.push('/checkout');
+            dispatch(toggleCartHidden());
+          }}
+        >
           GO TO CHECKOUT
         </CustomButton>
       ) : null}
